58fe21cb75b1ec6acf33d6972685d433ed694f06
Nick Ball <ballnikolas@yahoo.com>
2020-04-02 20:52:11 -0400

    database org

--- includes/header.php
+++ includes/header.php
@@ -17,10 +17,13 @@ else {
 <head>
     <meta charset="utf-8">
     <title>Notify || Not Spotify</title>
+    <link rel="icon" type="image/png" href="./assets/images/icons/favicon.png">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-
+<!-- Copyright 2020 Nick Ball -->
+<!-- Private project only for demonstration purposes -->
+<!-- No copyright infringement intended -->
     <div id="mainContainer">
         <div id="topContainer">
             <?php include("includes/navBarContainer.php"); ?>
--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php include("includes/header.php"); ?>
 
+<h1 class="pageHeadingBig">You Might Also Like</h1>
+
+<div class="gridViewContainer">
+    <?php
+        $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+
+        while($row = mysqli_fetch_array($albumQuery)) {
+            echo "<div class='gridViewItem'>
+                    <img src='" . $row['artworkPath'] . "'>
+
+                  <div class='gridViewInfo'>"
+                    . $row['title'] .
+                  "</div>
+                  </div>";
+        }
+    ?>
+
+</div>
 
 <?php include("includes/footer.php"); ?>
-                
