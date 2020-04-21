d47f7516963aa97352cfcd332e32e0dfe7b7fcb5
Nick Ball <ballnikolas@yahoo.com>
2020-04-21 19:58:38 -0400

    cont search page

--- search.php
+++ search.php
@@ -12,7 +12,27 @@ else {
 <div class="searchContainer">
 
     <h4>Search for an artist, album, or song</h4>
-    <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing...">
-
+    <!-- <input type="text" class="searchInput" value="" placeholder="Start typing..." onfocus="this.value = this.value"> -->
+    <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing..." onfocus="var val=this.value; this.value=''; this.value= val;" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">
 
 </div>
+
+<script>
+
+$(".searchInput").focus();
+
+$(function() {
+    var timer;
+
+
+    $(".searchInput").keyup(function() {
+        clearTimeout(timer);
+
+        timer = setTimeout(function() {
+            var val = $(".searchInput").val();
+            openPage("search.php?term=" + val);
+        }, 1000);
+    });
+});
+
+</script>
