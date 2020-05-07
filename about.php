a6c9848f3938e7c104c6e890f24fcb64983ef24b
Nick Ball <ballnikolas@yahoo.com>
2020-05-06 23:53:03 -0400

    finish dev

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php
+include("includes/includedFiles.php");
+?>
+
+<div id="aboutPageText">
+    <p>
+        To learn more about this instructor led web dev project, <a href="https://github.com/nick-ball/Notify">check out the README and all the code in the Github repository.</a>
+
+    </p>
+</div>
--- includes/navBarContainer.php
+++ includes/navBarContainer.php
@@ -26,7 +26,7 @@
                             <span role="link" tabindex="0" onclick="openPage('yourMusic.php')" class="navItemLink">Your Music</span>
                         </div>
                         <div class="navItem">
-                            <span role="link" tabindex="0" onclick="openPage('profile.php')" class="navItemLink">Your Profile</span>
+                            <span role="link" tabindex="0" onclick="openPage('settings.php')" class="navItemLink"><?php echo $userLoggedIn->getFirstAndLastName(); ?></span>
                         </div>
                         <div class="navItem">
                             <span role="link" tabindex="0" onclick="openPage('about.php')" class="navItemLink">About</span>
